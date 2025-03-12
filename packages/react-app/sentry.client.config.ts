435f29fdc9d90d56a9a60af9a96a1a1af1751f0c
Celso Cardenas <cardenascelso9@gmail.com>
2025-03-11 18:00:27 -0600

    add sentry config

---
+++ packages/react-app/sentry.client.config.ts
@@ -0,0 +1,25 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://4b91402757ec2334a34b5c0c856d44e3@o4506577784602624.ingest.us.sentry.io/4508961764409344",
+
+  // Add optional integrations for additional features
+  integrations: [
+    Sentry.replayIntegration(),
+  ],
+
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
